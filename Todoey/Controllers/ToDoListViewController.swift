3767f384d13fb178e5d481d9952be546ff6a3ae4
Jon Rees <jonrees69@aol.com>
2018-07-01 09:35:02 +0100

    Persisting data using Codeable

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+
+let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
 ////////////////////////
 //MARK:- Class variables
@@ -23,23 +25,7 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-            
-            itemArray = items
-            
-        }
+        loadItems()
 
     }
     
@@ -67,59 +53,102 @@ class ToDoListViewController: UITableViewController {
         return itemArray.count
     }
     
-    ///////////////////////////////////
-    //MARK - TableView Delegate Methods
+///////////////////////////////////
+//MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
     
-//////////////////////
+////////////////////////
 //MARK:- - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        // Set a texfield object for UIAlert
         var textField = UITextField()
         
+        // Set parameters for UIAlert
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
+        // Set button type for UIAlert
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             // What will happen once the user clicks the add item button on our UIAlert
             
+            // Initialise new item
             let newItem = Item()
+            
+            // Set the new item from textfield
             newItem.title = textField.text!
             
+            // Add new item to array
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
-            self.tableView.reloadData()
-            
-        }
+            // SAVE
+            self.saveItems()
         
         alert.addTextField { (alertTextField) in
             
             alertTextField.placeholder = "Crete new item"
             textField = alertTextField
             
+            }
         }
         
+        // Show UIAlert popup
         alert.addAction(action)
-        
+
         present(alert, animated: true, completion: nil)
         
     }
     
+//MARK:- Model Manipulation Methods
+        
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            
+            print("Error encoding item array, \(error)")
+            
+        }
+        
+        self.tableView.reloadData()
+        
+        
+    }
     
-    
-    
-    
+    func loadItems() {
+
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+                
+            } catch {
+                
+                print("Error in decoding file: \(error)")
+                
+            }
+        }
+        
+        
+    }
     
     
     
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-class Item {
+class Item: Codable {
     
-    var title = ""
-    var done = false
+    var title : String = ""
+    var done : Bool = false
 
 }
